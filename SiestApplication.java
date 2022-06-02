ba9821b7c7b92aefdc4ac19b5006a70a6062f777
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-06-02 12:19:10 -0600

    Create SiestApplication.java

---
+++ SiestApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiestApplication.class, args);
+	}
+
+}
