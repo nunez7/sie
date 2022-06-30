df0b150ae592318035be21320391e4c2dd3a804f
Donaji Anzaldo <67440408+Rhekhienth@users.noreply.github.com>
2022-06-30 10:59:07 -0600

    notificaciones profesor

--- repository/DosificacionCargaRepository.java
+++ repository/DosificacionCargaRepository.java
@@ -2,7 +2,9 @@ package edu.mx.utdelacosta.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import edu.mx.utdelacosta.model.CargaHoraria;
 import edu.mx.utdelacosta.model.Dosificacion;
@@ -13,5 +15,12 @@ public interface DosificacionCargaRepository extends CrudRepository<Dosificacion
 	DosificacionCarga findByDosificacionAndCargaHoraria (Dosificacion dosificacion, CargaHoraria cargaHoraria);
 	
 	List<DosificacionCarga> findByCargaHoraria(CargaHoraria cargaHoraria);
+	
+	// se obtiene el numero de cargas con dosificaciones sin entregar, notificaciones/profesor
+	@Query(value = "SELECT COUNT(*) "
+			+ "FROM cargas_horarias ch "
+			+ "LEFT JOIN dosificaciones_cargas dc ON dc.id_carga_horaria=ch.id "
+			+ "WHERE ch.id_profesor = :idProfesor and ch.id_periodo = :idPeriodo and dc.id IS NULL", nativeQuery = true)
+	Integer countNoEntregadas(@Param("idProfesor") Integer idProfesor, @Param("idPeriodo") Integer idPeriodo);
 	
 }
